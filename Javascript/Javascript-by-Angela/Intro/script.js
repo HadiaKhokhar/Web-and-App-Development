a67d778d3ec1a33d5643f689d11986699a5a842d
Hadia Khokhar <workwithhadiaa@outlook.com>
2024-08-13 18:29:54 +0500

    Started Javascript
    Learn  alerts , Data Type , Variables , Concatenation , slicing

---
+++ Javascript/Javascript-by-Angela/Intro/script.js
@@ -0,0 +1,43 @@
+Alerts
+alert("Hello");
+
+// Data types
+alert("String");
+alert(123); // Number
+alert(true) ;// Boolean
+
+alert(typeof "e")
+alert(typeof 1 )
+alert(typeof true )
+
+alert(2+3);
+alert(2==3);
+
+Variables
+prompt("What is your name?")
+
+var myName = prompt("What is your name?");
+alert(myName);
+alert("Your name is "+myName);
+
+myName = "hiba"
+alert(myName);
+
+// Variable Naming
+// ☒ stupid name
+// ☒ starting variable name with a number
+// ☒ naming a variable with a keyword 
+// ☒ using spaces and symbol 
+
+// ☑  using camel Casing
+// ☑  Only using number middle or after the in the name of the varible 
+// ☑  Only using dollar and inderscores 
+
+// Concatenation of strings
+
+var message = "hello"
+
+alert(message + " there, " + myName )
+
+// slicing
+alert(message.slice(1,4))
