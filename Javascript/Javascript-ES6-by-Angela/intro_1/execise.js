ac21f8338635372fd3c178562e448cb478d64d67
Hadia Khokhar <workwithhadiaa@outlook.com>
2024-08-18 22:48:01 +0500

    Updated

--- Javascript/Javascript-ES6-by-Angela/intro_1/execise.js
+++ Javascript/Javascript-ES6-by-Angela/intro_1/execise.js
@@ -1,84 +1,84 @@
 
 // 1.1
-// function test(){
+function test(){
 
-//     var a = "3";
-//     var b = "8";
+    var a = "3";
+    var b = "8";
     
 // my code here
-//     var c = a;
-//     a = b;
-//     b = c;
+    var c = a;
+    a = b;
+    b = c;
 
 
-//     alert("a is :" + a);
-//     alert("b is :" + b);
+    alert("a is :" + a);
+    alert("b is :" + b);
 
 
-// }
-// test(); 
+}
+test(); 
 
 // 1.2
-// var tweet = prompt("Enter your tweet:");
-// var tweetLength = tweet.length;
-// var total_character = 240;
-// var remaining_character = total_character - tweetLength
-// alert("you have written " + tweetLength + " characters and left with "+ remaining_character)
+var tweet = prompt("Enter your tweet:");
+var tweetLength = tweet.length;
+var total_character = 240;
+var remaining_character = total_character - tweetLength
+alert("you have written " + tweetLength + " characters and left with "+ remaining_character)
  
 // 1.3
-// var s_tweet = tweet.slice(0,148)
-// alert(s_tweet)
-// alert(s_tweet.length) 
+var s_tweet = tweet.slice(0,148)
+alert(s_tweet)
+alert(s_tweet.length) 
 
 //1.4 Casing In Text
 
-// var user_name = prompt("What's Your Name?");
-// user_name = user_name.slice(0,1).toUpperCase()+user_name.slice(1,user_name.length).toLowerCase();
-// alert("Hey! "+ user_name);
+var user_name = prompt("What's Your Name?");
+user_name = user_name.slice(0,1).toUpperCase()+user_name.slice(1,user_name.length).toLowerCase();
+alert("Hey! "+ user_name);
 
 //1.  Odd and Even Guess
-// var number =    parseInt(prompt("EVEN AND ODD GUESSER: \n \nEnter a Number"));
-// if (number % 2 == 0) {
-//     alert(number + " is an Even Number");
-// }
-// else {
-//     alert(number + " is an Odd Number");
-// }
+var number =    parseInt(prompt("EVEN AND ODD GUESSER: \n \nEnter a Number"));
+if (number % 2 == 0) {
+    alert(number + " is an Even Number");
+}
+else {
+    alert(number + " is an Odd Number");
+}
 
-// alert(number++)
+alert(number++)
 
 // Creating a Function for calculating a number of bottles a person can get
 
-// function numOfBottles(Money){
-//    var priceOfBottle = 1.5;
-//    var Bottles  =  Math.floor(Money/priceOfBottle);
-//    var change =  Money%priceOfBottle;
-//    alert("Yow will get " + Bottles +" Bottles of Milk and here is your change "+ change )
-// }
-// numOfBottles(parseInt(prompt("How much money do you have?")))
+function numOfBottles(Money){
+   var priceOfBottle = 1.5;
+   var Bottles  =  Math.floor(Money/priceOfBottle);
+   var change =  Money%priceOfBottle;
+   alert("Yow will get " + Bottles +" Bottles of Milk and here is your change "+ change )
+}
+numOfBottles(parseInt(prompt("How much money do you have?")))
 
 
 
-// Function to Coint how many days weeks and years left in your life time
-// // Years to Week Converter
-// function yearsToWeek(years){
-//     return (years * 52)
-// }
-// // Years to Days Converter
-// function yearsToDays(years){
-//     return (years * 365)
-// }
-// function lifeInDaysWeeksYears(age){
-//     idealAge = 90;
-//     // Life in Years
-//     var lifeInYears = idealAge - age;
-//     // Life in Weeks
-//     var lifeInWeeks = yearsToWeek(idealAge) - yearsToWeek(age)  ;
-//     // Life in Days
-//     var lifeInDays = yearsToDays(idealAge) - yearsToDays(age);
-//     alert(`You have ${lifeInDays} Days, ${lifeInWeeks} Weeks , ${lifeInYears} Years Left in Your Lifetime`)
-// }
-// lifeInDaysWeeksYears(12);
+// Function to Count how many days weeks and years left in your life time
+// Years to Week Converter
+function yearsToWeek(years){
+    return (years * 52)
+}
+// Years to Days Converter
+function yearsToDays(years){
+    return (years * 365)
+}
+function lifeInDaysWeeksYears(age){
+    idealAge = 90;
+    // Life in Years
+    var lifeInYears = idealAge - age;
+    // Life in Weeks
+    var lifeInWeeks = yearsToWeek(idealAge) - yearsToWeek(age)  ;
+    // Life in Days
+    var lifeInDays = yearsToDays(idealAge) - yearsToDays(age);
+    alert(`You have ${lifeInDays} Days, ${lifeInWeeks} Weeks , ${lifeInYears} Years Left in Your Lifetime`)
+}
+lifeInDaysWeeksYears(12);
 
 
 // BMI Calculator
--- Javascript/Javascript-ES6-by-Angela/intro_1/script.js
+++ Javascript/Javascript-ES6-by-Angela/intro_1/script.js
@@ -1,27 +1,27 @@
 // Alerts
-// alert("Hello");
+alert("Hello");
 
-// // Data types
-// alert("String");
-// alert(123); // Number
-// alert(true) ;// Boolean
+// Data types
+alert("String");
+alert(123); // Number
+alert(true) ;// Boolean
 
-// alert(typeof "e")
-// alert(typeof 1 )
-// alert(typeof true )
+alert(typeof "e")
+alert(typeof 1 )
+alert(typeof true )
 
-// alert(2+3);
-// alert(2==3);
+alert(2+3);
+alert(2==3);
 
-// Variables
-// prompt("What is your name?")
+Variables
+prompt("What is your name?")
 
-// var myName = prompt("What is your name?");
-// alert(myName);
-// alert("Your name is "+myName);
+var myName = prompt("What is your name?");
+alert(myName);
+alert("Your name is "+myName);
 
-// myName = "hiba"
-// alert(myName);
+myName = "hiba"
+alert(myName);
 
 // Variable Naming
 // ☒ stupid name
@@ -35,54 +35,54 @@
 
 // Concatenation of strings
 
-// var message = "hello"
+var message = "hello"
 
-// alert(message + " there, " + myName )
+alert(message + " there, " + myName )
 
 // slicing
-// alert(message.slice(1,4))
+alert(message.slice(1,4))
 
 
 
 // Arithematic operation
-// var a = 2;
-// var b = 45 - 6;
-// var c = 23 *3;
-// var d = 24/4;
-// var e = 23 % 4 ;   // Modulus the answer will be the remiander of the division
-// alert("a: "+ a +" b: " +b )
+var a = 2;
+var b = 45 - 6;
+var c = 23 *3;
+var d = 24/4;
+var e = 23 % 4 ;   // Modulus the answer will be the remiander of the division
+alert("a: "+ a +" b: " +b )
 
 // Increment and Decrement operators
-// a++;
-// b--;
-// alert("After the Increment and Decremen \na: "+ a +" b: " +b );
-// a+=b ;
-// alert("The answer of a+=b:"+ a);
+a++;
+b--;
+alert("After the Increment and Decremen \na: "+ a +" b: " +b );
+a+=b ;
+alert("The answer of a+=b:"+ a);
 
 // Creating and Calling functions
-// function evenAndOddIdentifier(){
-//     var number =    parseInt(prompt("EVEN AND ODD GUESSER: \n \nEnter a Number"));
-//     if (number % 2 == 0) {
-//         alert(number + " is an Even Number");
-//     }
-//     else {
-//         alert(number + " is an Odd Number");
-//     }
-//     alert(number++)
-// }
+function evenAndOddIdentifier(){
+    var number =    parseInt(prompt("EVEN AND ODD GUESSER: \n \nEnter a Number"));
+    if (number % 2 == 0) {
+        alert(number + " is an Even Number");
+    }
+    else {
+        alert(number + " is an Odd Number");
+    }
+    alert(number++)
+}
 
-// evenAndOddIdentifier();
+evenAndOddIdentifier();
 
 
 // Creating and giving parameter
-// function numOfBottles(Money){
-//     var price_of_bottle = 1.5;
-//     var change = Money%price_of_bottle; 
-//     var  num_bottles = (Money-change)/1.5 ;
-//     alert(num_bottles + " bottles of Milk and here is your change " + change);
+function numOfBottles(Money){
+    var price_of_bottle = 1.5;
+    var change = Money%price_of_bottle; 
+    var  num_bottles = (Money-change)/1.5 ;
+    alert(num_bottles + " bottles of Milk and here is your change " + change);
    
-// }
-// numOfBottles(5)
+}
+numOfBottles(5)
 
 
 
