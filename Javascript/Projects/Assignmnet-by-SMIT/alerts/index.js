352463b224287fac1cebb6c075f0e5d567db3d08
Hadia Khokhar <workwithhadiaa@outlook.com>
2024-08-30 21:54:42 +0500

    Completed Assignmnet (Alert) of JS

---
+++ Javascript/Projects/Assignmnet-by-SMIT/alerts/index.js
@@ -0,0 +1,17 @@
+// TODO : Write a script to greet your website visitor using JS alert box.
+alert("Greetings, Welcome to my webpage") ;
+
+// TODO : Write a script to display following message on your web page:
+alert("Error! Please Enter a valid Password.");
+
+// TODO : Write a script to display following message on your web page: (Hint : Use line break)
+alert("Welcome to JS Land \nHappy Coding");
+
+// TODO : Write a script to display following messages in sequence:
+alert("Welcome to JS Land");
+alert("Happy Coding!"); 
+
+// TODO: . Generate the following message through browser’s developer console: (Completed in Browser's Console)
+alert("Hello.. I can run JS through my browser's console");
+
+// TODO: Make use of alerts in your new/existing HTML & CSS project.(Completed) amd Practice placement of <script></script> element in following sections of your project in exercise 6: a. Head b. Body (before your page’s HTML)  c. Body (inside your page’s HTML)d. Body (after your page’s HTML)
