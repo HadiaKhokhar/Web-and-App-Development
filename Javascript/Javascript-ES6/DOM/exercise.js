f3b07f7ff02cef9e4ca8b73c4bc62f2b0d9c7314
Hadia Khokhar <workwithhadiaa@outlook.com>
2024-09-16 01:04:52 +0500

    Learn DOM in JS

---
+++ Javascript/Javascript-ES6/DOM/exercise.js
@@ -0,0 +1,13 @@
+// TO DO  select the third list item and changed its text
+document.querySelector("ul").children[2].innerHTML = "Hadia";
+
+// TO DO  select a tag in the the second list item
+document.querySelector(".list-group a").style.color = "orange"
+
+// Changing the bg color of tthe bytton
+document.querySelector("#btn").style.backgroundColor = "yellow"
+
+// Adding a class to a h1 with js
+document.querySelector("h1").classList.add("huge")
+
+
